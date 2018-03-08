899c7e3e4a8eaf0d1af36f510ec784e7d355b8b8
Ingmar Delsink <ingmardelsink@hotmail.com>
2018-03-08 10:59:59 +0100

    Fix relative path to abs path

--- replace-package-name.js
+++ replace-package-name.js
@@ -1,14 +1,14 @@
 /**
  * Replace the package name with any other name
  */
-
+const resolve = require('path').resolve
 const input = process.argv[2];
 const name =  process.argv[3]
 const output = process.argv[4] || process.argv[2];
 
 // The JSON file
-const o = require(input);
+const o = require(resolve(input));
 
 o.name = name;
 
-require("fs").writeFileSync(output, JSON.stringify(o, null, 2));
+require("fs").writeFileSync(resolve(output), JSON.stringify(o, null, 2));
